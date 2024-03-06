22b3eca237e17f776abcae58e6379c09c5e24423
Irfan <irfansheikh41297@gmail.com>
2024-03-06 15:03:26 +0530

    Revert "Revert "Revert "test"""
    This reverts commit c7b6cb62be6d67dea0f34873f54d6204a0d1026e.

--- src/App.js
+++ src/App.js
@@ -1,56 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import Alert from './Components/Alert';
-import About from './Components/About';
-import Navbar from './Components/Navbar';
-import TextForm from './Components/TextForm';
-import React, { useState } from 'react'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
 
 function App() {
-    const[mode, setMode] = useState('light')
-    const[alert, setAlert] = useState(null)
-    
-    const showAlert = (type, msg) => {
-      setAlert({
-        type:type,
-        msg:msg
-      })
-      setTimeout(() => {
-        setAlert(null)
-      }, 1500);
-    }
-
-    const toggleMode = ()=>{
-        if(mode === 'light'){
-         setMode('dark') 
-         document.body.style.backgroundColor = '#252F44'
-         showAlert('Success','Dark Mode Enabled')
-        }else{
-          setMode('light')
-          document.body.style.backgroundColor = 'white'
-          showAlert('Success','Dark Mode Disabled')
-      }
-    }
-
   return (
-    <>
-    <Router>
-      <Navbar title="TextUtils" mode = {mode} toggleMode = {toggleMode} />
-      <Alert alert = {alert}/>
-
-      <div className="container my-3">
-        <Routes>
-          <Route path="/about" element = {<About />}></Route>
-          <Route path="/" element = {<TextForm heading = "TextUtils - Analyze your text" mode = {mode}  showAlert={showAlert}/>}></Route>
-          </Routes>
-      </div>
-      
-    </Router>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
